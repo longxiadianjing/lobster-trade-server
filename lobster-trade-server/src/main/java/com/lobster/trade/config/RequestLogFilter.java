9ffb812f5ec3591eb7a4f37a74d5d22f1ba3b8dd
longxiadianjing <578097153@qq.com>
2026-05-10 09:32:23 +0800

    feat: add RequestLogFilter with @WebFilter for SSE /subscribe debugging

---
+++ lobster-trade-server/src/main/java/com/lobster/trade/config/RequestLogFilter.java
@@ -0,0 +1,32 @@
+package com.lobster.trade.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@Slf4j
+@Component
+@WebFilter(urlPatterns = "/*")
+public class RequestLogFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        String path = req.getRequestURI();
+        
+        if (path.contains("/subscribe")) {
+            System.out.println("[RequestLogFilter] BEFORE chain for: " + path + " token=" + req.getParameter("token"));
+        }
+        
+        chain.doFilter(request, response);
+        
+        if (path.contains("/subscribe")) {
+            System.out.println("[RequestLogFilter] AFTER chain for: " + path);
+        }
+    }
+}
